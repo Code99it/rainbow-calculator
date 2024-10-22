99fbc7cb6f3c67517e517b4b29826501d23745ff
cw <cw@abc.local>
2024-10-22 13:05:11 +0200

    Added an info box at the top left of the display to show indicators like DEG or M. Styled buttons with colors. Reorganized operators, with the plus button now at the bottom. Added output to the JavaScript console. Introduced a development box that logs and displays button inputs.

--- index.php
+++ index.php
@@ -30,40 +30,86 @@
     <main>
         <section id="home">
             <div class="calculator-container">
-                <div class="display" style="grid-column: span 4;">0.</div>
+                <div class="infobar">
+                    <div class="angle-info">DEG</div> 
+                    <div class="memory-info">M</div> 
+                </div>
+                <div id="displaybar" class="display" style="grid-column: span 4;">0</div>
                 <div class="rainbow-line" style="grid-column: span 4;"></div>
 
-                <div class="item">AC</div>
-                <div class="item">C</div>
-                <div class="item">M+</div>
-                <div class="item">MR</div>
+                <button onclick="keypress(this)" class="item clear">AC</button>
+                <button onclick="keypress(this)" class="item clear">C</button>
+                <button onclick="keypress(this)" class="item memory">M+</button>
+                <button onclick="keypress(this)" class="item memory">MR</button>
 
-                <div class="item">7</div>
-                <div class="item">8</div>
-                <div class="item">9</div>
-                <div class="item">+</div>
+                <button onclick="keypress(this)" class="item number">7</button>
+                <button onclick="keypress(this)" class="item number">8</button>
+                <button onclick="keypress(this)" class="item number">9</button>
+                <button onclick="keypress(this)" class="item operator">:</button>
 
-                <div class="item">4</div>
-                <div class="item">5</div>
-                <div class="item">6</div>
-                <div class="item">-</div>
+                <button onclick="keypress(this)" class="item number">4</button>
+                <button onclick="keypress(this)" class="item number">5</button>
+                <button onclick="keypress(this)" class="item number">6</button>
+                <button onclick="keypress(this)" class="item operator">x</button>
 
-                <div class="item">1</div>
-                <div class="item">2</div>
-                <div class="item">3</div>
-                <div class="item">x</div>
+                <button onclick="keypress(this)" class="item number">1</button>
+                <button onclick="keypress(this)" class="item number">2</button>
+                <button onclick="keypress(this)" class="item number">3</button>
+                <button onclick="keypress(this)" class="item operator">-</button>
 
-                <div class="item">0</div>
-                <div class="item">.</div>
-                <div class="item">=</div>
-                <div class="item">:</div>
+                <button onclick="keypress(this)" class="item number">0</button>
+                <button onclick="keypress(this)" class="item number">.</button>
+                <button onclick="keypress(this)" class="item equals">=</button>
+                <button onclick="keypress(this)" class="item operator">+</button>
            </div>
+        </section>
+        <section>
+            <div>
+                <div id="devbox"></div>
+            </div>
         </section>
     </main>
 
     <footer>
         <p></p>
     </footer>
+
+    <script type="text/javascript">
+
+        const devbox = document.getElementById('devbox');
+        const displaybar = document.getElementById('displaybar');
+        const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+        let readytext = "READY.";
+        devbox.innerHTML = readytext + "<br>";
+        console.log(readytext)
+
+        function keypress(button) {
+
+            let key = button.textContent;
+            let devmessage = key + ' pressed';
+
+            // devbox
+            devbox.innerHTML += devmessage + '<br>';
+            console.log(devmessage);
+            devbox.scrollTop = devbox.scrollHeight;
+
+            // clear display for next input when 0 is displayed
+            if (displaybar.innerHTML == '0') displaybar.innerHTML = '';
+
+            // keys 0 - 9 pressed
+            if (digits.includes(key)) {
+                displaybar.innerHTML += key;
+            }
+
+            // key AC pressed
+            if (key == 'AC') {
+                displaybar.innerHTML = '0';
+            }
+
+        }
+
+    </script>
 
 </body>
 </html>
